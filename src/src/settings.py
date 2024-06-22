3555971b88106e9ccad8d815b3d761fd8b926c00
ignurof <ignurof@ignurof.com>
2024-06-22 10:13:18 +0200

    Fix incorrect app config name in settings
    Issue due to copying over this section from my todo app test project

--- src/src/settings.py
+++ src/src/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # personal apps
-    "tcapp.apps.TodoConfig",
+    "tcapp.apps.TcappConfig",
 ]
 
 MIDDLEWARE = [
