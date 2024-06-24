b0a0b90b6c79e9b60d010a8a3fd9f52977643f03
ignurof <ignurof@ignurof.com>
2024-06-24 11:30:40 +0200

    Add some project setup

--- src/tcapp/admin.py
+++ src/tcapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import *
+models = ["topic", "comment"]
+model_list = []
+
+# https://stackoverflow.com/questions/24940545/how-to-import-modules-given-a-list-of-their-names
+for model in models:
+    pass
+    # globals()[model] = __import__(model)
+    # admin.site.register(globals()[model])
